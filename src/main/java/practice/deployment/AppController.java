f95b4f16a0e5b8d47053a95c4c32a7d69e417426
u <mjkim2304@gmail.com>
2025-06-06 07:11:38 +0900

    [fix] yml datasource error

--- src/main/java/practice/deployment/AppController.java
+++ src/main/java/practice/deployment/AppController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "Hello, World!";
     }
